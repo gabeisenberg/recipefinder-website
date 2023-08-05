29961c81a2a9961530e6d384d69a545ccda1b9de
Kaniel Vicencio <kanielvicencio@gmail.com>
2023-08-05 12:49:12 -0400

    reorganized files

--- public/fun.js
+++ public/fun.js
@@ -26,48 +26,64 @@ async function postData(url, data) {
     return response;
 }
 
+async function getData(url) {
+    const response = await fetch(url, {
+        method: "GET",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json"
+        },
+    });
+    return response;
+}
+
+let currIndex = 0;
+let gRecipes = [];
 function onSubmit() {
+    showLoad();
     const list = displayInput();
     const recipeObj = {
         "neededIngredients": list
     };
-    postData("http://localhost:3000/recipes", recipeObj).then((data) => {
-        console.log(data); // JSON data parsed by `data.json()` call
+    postData('http://localhost:3000/recipes', recipeObj).then((data) => {
+        data.json().then(list => {
+            let recipes = JSON.parse(list)
+            gRecipes = recipes
+            console.log(recipes)
+            currIndex = 0
+            showRecipes(recipes)
+        }); // JSON data parsed by `data.json()` call
     });
-    showRecipes(list);
+    hideLoad();
 }
 
-/*function revealBox() { //isnt working
-    const box = document.getElementById("res");
-    box.classList.toggle("hidden");
-}*/
+function showLoad() {
+    document.getElementById('load').style.visibility = 'visible';
+}
 
-let currIndex=0;
-const recipe1 = [["Milk","Eggs"], ["Sugar", "Flour", "Syrup"], ["Heat", "Pour", "Flip", "Serve"], "Pancakes"];
-const recipe2 = [["Lettuce", "Spinach"], ["Onions", "Tomatoes"], ["Chop", "Mix", "Serve"], "Salad"];
-const recipe3 = [["Flour, Eggs"], ["Sugar", "Sprinkles", "Frosting"], ["Preheat", "Mix", "Pour", "Bake", "Frost"], "Cake"];
-const recipes = [recipe1, recipe2, recipe3];
+function hideLoad() {
+    document.getElementById('load').style.visibility = 'hidden';
+}
 
-function showRecipes(...recipeList) {
-    //const list = displayInput();
-    //import Recipe from './sort.js'; , import doesnt work
-    showRecipe();
+function showRecipes(recipeList) {
+    console.log(recipeList[currIndex]);
+    showRecipe(recipeList[currIndex]);
 }
 
 function goBack() {
-    if (currIndex>0) {
+    if (currIndex > 0) {
         --currIndex;
     }
     clearList();
-    showRecipe();
+    showRecipe(gRecipes[currIndex]);
 }
 
 function goNext() {
-    if (currIndex<2) { //size of recipes - 1, not working
+    if (currIndex < 5) { //size of recipes page
         ++currIndex;
     }
     clearList();
-    showRecipe();
+    showRecipe(gRecipes[currIndex]);
 }
 
 function clearList() {
@@ -77,14 +93,15 @@ function clearList() {
     dir.innerHTML = "";
 }
 
-function showRecipe() {
+function showRecipe(recipe) {
     //show name
     const currentRecipe = document.getElementById("current-recipe");
-    currentRecipe.textContent = recipes[currIndex][3];
+    currentRecipe.textContent = recipe.name;
     //show ingredients
     const currentStuff = document.getElementById("ingredients");
-    const orderList = document.createElement('ol');
-    recipes[currIndex][0].forEach(item => {
+    const orderList = document.createElement('ul');
+    console.log(recipe.ingredients);
+    recipe.ingredients.forEach(item => {
         const it = document.createElement('li');
         it.textContent = item;
         orderList.appendChild(it);
@@ -93,10 +110,18 @@ function showRecipe() {
     //show directions
     const currDirections = document.getElementById("directions");
     const dirList = document.createElement('ol');
-    recipes[currIndex][2].forEach(item => {
+    console.log(recipe.directions);
+    recipe.directions.forEach(item => {
         const it = document.createElement('li');
         it.textContent = item;
         dirList.appendChild(it);
     });
     currDirections.appendChild(dirList);
+    //show match
+    const currentMatch = document.getElementById("match");
+    const temp = parseFloat(recipe.ratio) * 100;
+    currentMatch.textContent = temp.toString() + "% match!";
+    //display titles
+    document.getElementById("iTitle").textContent = "Ingredients:";
+    document.getElementById("iDir").textContent = "Directions:";
 }
